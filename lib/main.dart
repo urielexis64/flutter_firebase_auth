9b0099f28af4c08f43aa4fa996c2276cdfab2f83
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-16 22:15:19 -0600

    feat: :sparkles: add app routes
    close #1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_auth/src/pages/HomePage.dart';
+import 'package:flutter_firebase_auth/src/pages/HuaweiCheckPage.dart';
+import 'package:flutter_firebase_auth/src/pages/UserProfilePage.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +10,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Firebase Auth',
+      initialRoute: ' /home',
+      routes: {
+        '/home': (_) => HomePage(),
+        '/huaweiCheck': (_) => HuaweiCheckPage(),
+        '/userProfile': (_) => UserProfilePage(),
+      },
     );
   }
 }
