5aaf90d54333e5fd0df2cb4649b3115267460a0b
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-16 23:24:21 -0600

    feat: :sparkles: add FacebookUser model
    close #3

--- lib/src/models/FacebookUser.dart
+++ lib/src/models/FacebookUser.dart
@@ -1,1 +1,23 @@
-class FacebookUser {}
+import 'package:flutter/material.dart';
+
+class FacebookUser {
+  String name;
+  String email;
+  String id;
+  String pictureUrl;
+  FacebookUser({
+    @required this.name,
+    @required this.email,
+    @required this.id,
+    @required this.pictureUrl,
+  });
+
+  factory FacebookUser.fromJson(Map<String, dynamic> parsedJson) {
+    return FacebookUser(
+      name: parsedJson['name'],
+      email: parsedJson['email'],
+      id: parsedJson['id'],
+      pictureUrl: parsedJson['picture']['data']['url'],
+    );
+  }
+}
