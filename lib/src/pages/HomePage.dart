f0fe3bafd26b97eab6b6a2b706ce1a6c9296adc8
Uriel Aispuro <urielalexis64@gmail.com>
2021-06-16 23:39:03 -0600

    feat: :sparkles: add OS name
    close #5

--- lib/src/pages/HomePage.dart
+++ lib/src/pages/HomePage.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Firebase Auth'),
+        centerTitle: true,
+      ),
+      body: Container(
+        height: double.infinity,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('OS: ${Platform.operatingSystem}')],
+        ),
+      ),
+    );
   }
 }
